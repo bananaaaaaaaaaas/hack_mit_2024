687beb625859e0205458bd8fc242c7e18d571117
Nathan Cho <chonachoz@gmail.com>
2024-09-14 17:56:05 -0400

    add reader

---
+++ book-reader/components/Reader.tsx
@@ -0,0 +1,8 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function Reader() {
+
+}
