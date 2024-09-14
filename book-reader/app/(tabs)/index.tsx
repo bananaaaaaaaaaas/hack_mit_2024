0423a326e84cab80afd58f84186b6fccc91b6855
Nathan Cho <chonachoz@gmail.com>
2024-09-14 18:01:28 -0400

    call

--- book-reader/app/(tabs)/index.tsx
+++ book-reader/app/(tabs)/index.tsx
@@ -3,11 +3,13 @@ import { Image, StyleSheet, Platform } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
+import { ImageReaderWithChat } from '@/components/Reader'
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
+    <ImageReaderWithChat />
+/*    <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
@@ -46,7 +48,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
-    </ParallaxScrollView>
+    </ParallaxScrollView>*/
   );
 }
 
